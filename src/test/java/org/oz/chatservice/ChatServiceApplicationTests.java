a969b30c05defd208c79017acfca27e11696385c
byunsp3610@gmail.com <byunsp3610@gmail.com>
2025-03-18 22:27:33 +0900

    feat: init project

---
+++ src/test/java/org/oz/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oz.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
